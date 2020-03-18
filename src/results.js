3feed33d23ece76ce63d3bccdb67c7eb1a727997
ReservePhilipp <philipp@aaholm.com>
2020-03-18 22:22:56 +0100

    Update results.js

--- src/results.js
+++ src/results.js
@@ -61,7 +61,7 @@ export const updateCount = () => {
     })
 
     if (isDesktop) {
-      RUN.tick % 2 === 0 && updateGraph()
+      RUN.tick % 3 === 0 && updateGraph()
     } else {
       RUN.tick % 4 === 0 && updateGraph()
     }
